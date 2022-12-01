d47144b91103d14676defa55d06dfe3cb0241725
ChrisChen114 <chen.qianfu114@gmail.com>
2022-12-01 00:22:27 -0800

    fix(server): fix bugs
    fix bugs

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended: true})) //请求体参数是name=tom&pwd=1
 app.use(express.json()) // 请求体参数是json结构：{name: tom,pwd:123}
 // 声明使用解析cookie数据的中间件
 const cookieParser = require('cookie-parser')
-app.use(cookieParser)
+app.use(cookieParser())
 // 声明使用路由器中间件
 const indexRouter = require('./routers')
 app.use('/api', indexRouter) //
@@ -25,30 +25,30 @@ const fs = require('fs')
 
 // 必须在路由器中间之后声明使用
 // 问题：使用BrowserRouter，刷新某个路由路径时，会出现 404 的错误
-app.use((req,res)=>{
-    fs.readFile(__dirname + '/public/index.html',(err,data)=>{
-        if (err){
+app.use((req, res) => {
+    fs.readFile(__dirname + '/public/index.html', (err, data) => {
+        if (err) {
             console.log(err)
             res.send('后台错误')
-        }else{
-            res.writeHead(200,{
-                'Content-Type':'text/html; charset=utf-8',
+        } else {
+            res.writeHead(200, {
+                'Content-Type': 'text/html; charset=utf-8',
             });
-            res.send(data)
+            res.end(data)
         }
     })
 })
 
 // 通过mongoose连接数据库
-mongoose.connect('mongodb://localhost/server_db2',{useNewUrlParser:true})
-    .then(()=>{
+mongoose.connect('mongodb://localhost/server_db2', {useNewUrlParser: true})
+    .then(() => {
         console.log('连接数据库成功!!!')
         // 只有当连接上数据库后才去启动服务器
-        app.listen('5000',()=>{
+        app.listen('5000', () => {
             console.log('服务器启动成功，请访问：http://localhost:5000')
         })
     })
-    .catch(error=>{
-        console.error('连接数据库失败',error)
+    .catch(error => {
+        console.error('连接数据库失败', error)
     })
 

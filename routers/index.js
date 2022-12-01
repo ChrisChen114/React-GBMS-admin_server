995c700b1edc1900098ca62eb251997ca2dad671
ChrisChen114 <chen.qianfu114@gmail.com>
2022-12-01 10:23:58 -0800

    feat(router): add new routers
    add new routers

--- routers/index.js
+++ routers/index.js
@@ -83,7 +83,7 @@ router.post('/manage/user/add', (req, res) => {
 // 3. 更新用户
 router.post('/manage/user/update', (req, res) => {
     const user = req.body // 必传-_id，可选-username, phone, email, role_id
-    UserModel.findByIdAndUpdate({_id: user._id}, user)// 此处已更新。 findByIdAndUpdate()函数用于查找匹配的文档，根据更新arg对其进行更新，传递任何选项，然后将找到的文档(如果有)返回给回调。
+    UserModel.findOneAndUpdate({_id: user._id}, user)// 此处已更新。 findOneAndUpdate()函数用于查找匹配的文档，根据更新arg对其进行更新，传递任何选项，然后将找到的文档(如果有)返回给回调。
         .then(oldUser => {
             const data = Object.assign(oldUser, user) // 此处的合并更新，将返回给前端，不用从数据库查询了。 Object.assign(target,source)=>主要用于对象合并，将源对象中的属性复制到目标对象中，他将返回目标对象; 如果有同名属性的话，后面的属性值会覆盖前面的属性值
             // 返回
@@ -145,8 +145,200 @@ router.get('/manage/user/list', (req, res) => {
 
 
 // 7. 添加分类
+router.post('/manage/category/add', (req, res) => {
+    const {categoryName, parentId} = req.body
+    CategoryModel.create({name: categoryName, parentId: parentId || '0'}) // 父分类id或一级分类（有的一级分类下面，没有子分类）
+        .then(category => {
+            res.send({status: 0, data: category})
+        })
+        .catch(error => {
+            console.error('添加分类异常', error)
+            res.send({status: 1, msg: '添加分类异常，请重新尝试'})
+        })
+})
 
 // 6. 获取分类列表 (获取一级或某个二级分类列表)
+router.get('/manage/category/list', (req, res) => {
+    const parentId = req.query.parentId || '0' // req.query 从请求参数中获取属性值
+    CategoryModel.find({parentId})
+        .then(categorys => {
+            res.send({status: 0, data: categorys})
+        })
+        .catch(error => {
+            console.error('获取分类列表异常', error)
+            res.send({status: 1, msg: '获取分类列表异常，请重新尝试'})
+        })
+})
+
+// 8. 更新分类名称
+router.post('/manage/category/update', (req, res) => {
+    const {categoryId, categoryName} = req.body
+    CategoryModel.findOneAndUpdate({_id: categoryId}, {name: categoryName}) // 后一个是待更新分类名称
+        .then(oldCategory => {
+            res.send({status: 0})
+        })
+        .catch(error => {
+            console.error('更新分类名称异常', error)
+            res.send({status: 1, msg: '更新分类名称异常，请重新尝试'})
+        })
+})
+
+// 9. 根据分类ID获取分类
+router.get('/manage/category/info', (req, res) => {
+    const categoryId = req.query.categoryId // 传入分类id
+    CategoryModel.findOne({_id: categoryId})
+        .then(category => {
+            res.send({status: 0, data: category})
+        })
+        .catch(error => {
+            console.error('获取分类信息异常', error)
+            res.send({status: 1, msg: '获取分类信息异常，请重新尝试'})
+        })
+})
+
+
+// 12. 添加产品
+router.post('/manage/product/add', (req, res) => {
+    const product = req.body    // 传了一个对象product过来
+    ProductModel.create(product)
+        .then(product => {
+            res.send({status: 0, data: product})
+        })
+        .catch(error => {
+            console.error('添加产品异常', error)
+            res.send({status: 1, msg: '添加产品异常，请重新尝试'})
+        })
+})
+
+// 10. 获取产品分页列表
+router.get('/manage/product/list', (req, res) => {
+    const {pageNum, pageSize} = req.query
+    ProductModel.find({})
+        .then(products => {
+            res.send({status: 0, data: pageFilter(products, pageNum, pageSize)})
+        })
+        .catch(error => {
+            console.error('获取商品列表异常', error)
+            res.send({status: 1, msg: '获取商品列表异常，请重新尝试'})
+        })
+})
+
+// 11. 搜索产品列表
+router.get('/manage/product/search', (req, res) => {
+    const {pageNum, pageSize, searchName, productName, productDesc} = req.query
+    // console.log('productName ', productName) // undefined   =>测试用
+    // console.log('productDesc ', productDesc) // T 搜索的内容
+    let condition = {}
+    if (productName) {
+        condition = {name: new RegExp(`^.*${productName}.*$`)}
+    } else if (productDesc) {
+        condition = {name: new RegExp(`^.*${productDesc}.*$`)}
+    }
+    ProductModel.find(condition)
+        .then(products => {
+            res.send({status: 0, data: pageFilter(products, pageNum, pageSize)})
+        })
+        .catch(error => {
+            console.error('搜索商品列表异常', error)
+            res.send({status: 1, msg: '搜索商品列表异常，请重新尝试'})
+        })
+})
+
+// 13. 更新产品
+router.post('/manage/product/update', (req, res) => {
+    const product = req.body
+    ProductModel.findOneAndUpdate({_id: product._id}, product) // 用后面的更新前面的
+        .then(oldProduct => {
+            res.send({status: 0})
+        })
+        .catch(error => {
+            console.error('更新商品异常', error)
+            res.send({status: 1, msg: '更新商品名称异常，请重新尝试'})
+        })
+})
+
+// 14. 更新产品状态（上架/下架）
+router.post('/manage/product/updateStatus', (req, res) => {
+    const {productId, status} = req.body
+    ProductModel.findOneAndUpdate({_id: productId}, {status}) // status商品状态： 1：在售，2：下架了
+        .then(oldProduct => {
+            res.send({status: 0})
+        })
+        .catch(error => {
+            console.error('更新产品状态异常', error)
+            res.send({status: 1, msg: '更新产品状态异常，请重新尝试'})
+        })
+})
+
+
+// 17. 添加角色
+router.post('/manage/role/add', (req, res) => {
+    const {roleName} = req.body
+    RoleModel.create({name: roleName})
+        .then(role => {
+            res.send({status: 0, data: role})
+        })
+        .catch(error => {
+            console.error('添加角色异常', error)
+            res.send({status: 1, msg: '添加角色异常，请重新尝试'})
+        })
+})
+
+// 18. 获取角色列表
+router.get('/manage/role/list', (req, res) => {
+    RoleModel.find()
+        .then(roles => {
+            res.send({status: 0, data: roles})
+        })
+        .catch(error => {
+            console.error('获取角色列表异常', error)
+            res.send({status: 1, msg: '获取角色列表异常，请重新尝试'})
+        })
+})
+
+// 19. 更新角色（设置权限）
+router.post('/manage/role/update', (req, res) => {
+    const role = req.body
+    role.auth_time = Date.now()
+    RoleModel.findOneAndUpdate({_id: role._id}, role)
+        .then(oldRole => {
+            // console.log('---',oldRole.doc) // what is the print  //--- undefined
+            // res.send({status: 0, data: {...oldRole.doc, ...role}}) //并没有得到api文档里的结果
+            const data = Object.assign(oldRole, role) // Chris 增加的这两条，看着能得到正确结果
+            res.send({status: 0, data}) //
+        })
+        .catch(error => {
+            console.error('更新角色异常', error)
+            res.send({status: 1, msg: '更新角色异常，请重新尝试'})
+        })
+})
+
+
+/*
+* 得到指定数组的分页信息对象
+* */
+function pageFilter(arr, pageNum, pageSize) {
+    pageNum = pageNum * 1  // 转为数值
+    pageSize = pageSize * 1
+    const total = arr.length
+    const pages = Math.floor((total + pageSize - 1) / pageSize)
+    const start = pageSize * (pageNum - 1)
+    const end = start + pageSize <= total ? start + pageSize : total
+    const list = []
+    for (var i = start; i < end; i++) {
+        list.push(arr[i])
+    }
+
+    return {
+        pageNum,
+        total,
+        pages,
+        pageSize,
+        list
+    }
+}
+
+require('./file-upload')(router)
 
 module.exports = router
 
